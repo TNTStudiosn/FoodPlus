abfe00874b4fccf6b8b94e4f4cbf899d803b1e23
TNTStudios <tntstudiosn@gmail.com>
2025-02-08 01:19:21 -0600

    1

--- src/main/java/com/TNTStudios/foodplus/Foodplus.java
+++ src/main/java/com/TNTStudios/foodplus/Foodplus.java
@@ -1,15 +1,10 @@
 package com.TNTStudios.foodplus;
 
-import com.TNTStudios.foodplus.recipe.FoodPotionRecipeProvider;
-import com.TNTStudios.foodplus.recipe.FoodPotionRecipeSerializer;
 import net.fabricmc.api.ModInitializer;
 
 public class Foodplus implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        FoodPotionRecipeSerializer.register();
-        FoodPotionRecipeProvider.register((recipe) -> {});
     }
-
 }
